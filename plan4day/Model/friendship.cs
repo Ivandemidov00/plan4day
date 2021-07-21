3bf209e21ae00e3d590a43efca94878f27c7577e
ivan.demidov <ivan.demidov00@mail.ru>
2021-07-21 18:35:03 +0500

    init

---
+++ plan4day/Model/friendship.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace plan4day.Model
+{
+    public class friendship
+    {
+        public Int32 id;
+        public Int32 userId;
+        public Int32 friendId;
+    }
+}
---
+++ plan4day/Model/user.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace plan4day.Model
+{
+    public class user
+    {
+        public Int32 id;
+        public String email;
+        public String name;
+    }
+}
